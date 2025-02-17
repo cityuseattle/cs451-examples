97aa710caa6925139f6b9713cc9f4f91c4be79c0
Clark Ngo <30854108+clarkngo@users.noreply.github.com>
2025-02-17 04:43:17 -0800

    Update App.js

--- Module08/CitiesApp/App.js
+++ Module08/CitiesApp/App.js
@@ -1,5 +1,4 @@
-// Updated App.js for tab- and stack-driven navigations
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -9,66 +8,90 @@ LogBox.ignoreLogs([
   'Non-serializable values were found in the navigation state',
 ]);
 
-import Cities from './src/Cities/Cities'
-import City from './src/Cities/City'
-import AddCity from './src/AddCity/AddCity'
-
-import { colors } from './src/theme'
+import Cities from './src/Cities/Cities';
+import City from './src/Cities/City';
+import AddCity from './src/AddCity/AddCity';
+import { colors } from './src/theme';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-function CitiesStackScreen ({navigation, route}){
-    return (
-      <Stack.Navigator screenOptions={{
+function CitiesStackScreen({ navigation, route, cities, addCity, addLocation }) {
+  return (
+    <Stack.Navigator
+      screenOptions={{
         headerStyle: {
-          backgroundColor: colors.primary
+          backgroundColor: colors.primary,
         },
-        headerTintColor: '#fff'
-      }}>
-      <Stack.Screen name="Cities" component={Cities} initialParams={{
-        cities: route.params.cities,
-        addCity: route.params.addCity,
-        addLocation: route.params.addLocation}} />
-      <Stack.Screen name="City" component={City} initialParams={{
-        cities: route.params.cities,
-        addCity: route.params.addCity,
-        addLocation: route.params.addLocation}}/>
-      </Stack.Navigator>
-    );
+        headerTintColor: '#fff',
+      }}
+    >
+      <Stack.Screen
+        name="Cities"
+        children={(props) => (
+          <Cities {...props} cities={cities} addCity={addCity} addLocation={addLocation} />
+        )}
+      />
+      <Stack.Screen
+        name="City"
+        children={(props) => (
+          <City {...props} cities={cities} addCity={addCity} addLocation={addLocation} />
+        )}
+      />
+    </Stack.Navigator>
+  );
 }
 
 export default class App extends Component {
   state = {
-    cities: []
+    cities: [],
+  };
 
-  }
   addCity = (city) => {
-    const cities = this.state.cities
-    cities.push(city)
-    this.setState({ cities })
-  }
+    this.setState((prevState) => ({
+      cities: [...prevState.cities, { ...city, locations: [] }],
+    }));
+  };
+
   addLocation = (location, city) => {
-    const index = this.state.cities.findIndex(item => {
-      return item.id === city.id
-    })
-    const chosenCity = this.state.cities[index]
-    chosenCity.locations.push(location)
+    const index = this.state.cities.findIndex((item) => item.id === city.id);
+    const updatedCity = { ...this.state.cities[index], locations: [...this.state.cities[index].locations, location] };
+
     const cities = [
       ...this.state.cities.slice(0, index),
-      chosenCity,
-      ...this.state.cities.slice(index + 1)
-    ]
-    this.setState({
-      cities
-    })
-  }
+      updatedCity,
+      ...this.state.cities.slice(index + 1),
+    ];
+
+    this.setState({ cities });
+  };
+
   render() {
     return (
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="CitiesNav" initialParams={{cities: this.state.cities,addCity: this.addCity,addLocation: this.addLocation}} component={CitiesStackScreen} />
-          <Tab.Screen name="AddCity"   initialParams={{cities: this.state.cities,addCity: this.addCity,addLocation: this.addLocation}} component={AddCity} />
+          <Tab.Screen
+            name="Cities"
+            children={(props) => (
+              <CitiesStackScreen
+                {...props}
+                cities={this.state.cities}
+                addCity={this.addCity}
+                addLocation={this.addLocation}
+              />
+            )}
+          />
+          <Tab.Screen
+            name="AddCity"
+            children={(props) => (
+              <AddCity
+                {...props}
+                cities={this.state.cities}
+                addCity={this.addCity}
+                addLocation={this.addLocation}
+              />
+            )}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     );
