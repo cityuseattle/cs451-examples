f7abb33b7d563e9803925ebc8f242737f47610db
Clark Ngo <30854108+clarkngo@users.noreply.github.com>
2025-02-17 04:42:38 -0800

    Update City.js

--- Module08/CitiesApp/Cities/City.js
+++ Module08/CitiesApp/Cities/City.js
@@ -1,74 +1,68 @@
-// Listing 6.9 City route (functionality)
-import React from 'react'
-import { View, Text, StyleSheet, ScrollView, TouchableWithoutFeedback, TextInput, TouchableOpacity} from 'react-native'
-
-import CenterMessage from '../components/CenterMessage'
-import { colors } from '../theme'
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import CenterMessage from '../components/CenterMessage';
+import { colors } from '../theme';
 
 class City extends React.Component {
-  static navigationOptions = (props) => {
-    const { city } = props.route.params
-    return {
-      title: city.city,
-      headerTitleStyle: {
-        color: 'white',
-        fontSize: 20,
-        fontWeight: '400'
-      }
-    }
-  }
   state = {
     name: '',
-    info: ''
-  }
+    info: '',
+  };
+
   onChangeText = (key, value) => {
-    this.setState({
-      [key]: value
-    })
-  }
+    this.setState({ [key]: value });
+  };
+
   addLocation = () => {
-    if (this.state.name === '' || this.state.info === '') return
-    const { city } = this.props.route.params
-    const location = {
-      name: this.state.name,
-      info: this.state.info
-    }
-    this.props.route.params.addLocation(location, city)
-    this.setState({ name: '', info: '' })
-  }
+    const { addLocation, city } = this.props.route.params;
+    const { name, info } = this.state;
+
+    if (name === '' || info === '') return;
+
+    const location = { name, info };
+
+    addLocation(location, city);
+
+    this.setState({ name: '', info: '' });
+  };
+
   render() {
-    const { city } = this.props.route.params
-    console.log('props: ', this.props)
+    const { cities, city } = this.props.route.params;
+
+    // Derive the updated version of the city from the global cities array
+    const updatedCity = cities.find((item) => item.id === city.id) || city;
+
     return (
       <View style={{ flex: 1 }}>
-        <ScrollView contentContainerStyle={[!city.locations.length && { flex: 1 }]}>
-          <View style={[styles.locationsContainer, !city.locations.length && { flex: 1, justifyContent: 'center' }]}>
-            {
-              !city.locations.length && <CenterMessage message='No locations for this city!' />
-            }
-            {
-              city.locations.map((location, index) => (
-                <View key={index} style={styles.locationContainer}>
-                  <Text style={styles.locationName}>{location.name}</Text>
-                  <Text style={styles.locationInfo}>{location.info}</Text>
-                </View>
-              ))
-            }
+        <ScrollView contentContainerStyle={[!updatedCity.locations.length && { flex: 1 }]}>
+          <View
+            style={[
+              styles.locationsContainer,
+              !updatedCity.locations.length && { flex: 1, justifyContent: 'center' },
+            ]}
+          >
+            {!updatedCity.locations.length && <CenterMessage message="No locations for this city!" />}
+            {updatedCity.locations.map((location, index) => (
+              <View key={index} style={styles.locationContainer}>
+                <Text style={styles.locationName}>{location.name}</Text>
+                <Text style={styles.locationInfo}>{location.info}</Text>
+              </View>
+            ))}
           </View>
         </ScrollView>
         <TextInput
-          onChangeText={val => this.onChangeText('name', val)}
-          placeholder='Location name'
+          onChangeText={(val) => this.onChangeText('name', val)}
+          placeholder="Location name"
           value={this.state.name}
           style={styles.input}
-          placeholderTextColor='white'
+          placeholderTextColor="white"
         />
         <TextInput
-          onChangeText={val => this.onChangeText('info', val)}
-          placeholder='Location info'
+          onChangeText={(val) => this.onChangeText('info', val)}
+          placeholder="Location info"
           value={this.state.info}
           style={[styles.input, styles.input2]}
-          placeholderTextColor='white'
+          placeholderTextColor="white"
         />
         <View style={styles.buttonContainer}>
           <TouchableOpacity onPress={this.addLocation}>
@@ -78,16 +72,13 @@ class City extends React.Component {
           </TouchableOpacity>
         </View>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
   locationsContainer: {
-    paddingBottom: 104
+    paddingBottom: 104,
   },
   input: {
     height: 50,
@@ -97,37 +88,37 @@ const styles = StyleSheet.create({
     position: 'absolute',
     width: '100%',
     bottom: 104,
-    left: 0
+    left: 0,
   },
   input2: {
-    bottom: 52
+    bottom: 52,
   },
   buttonContainer: {
     position: 'absolute',
     bottom: 0,
     left: 0,
-    width: '100%'
+    width: '100%',
   },
   button: {
     height: 50,
     backgroundColor: colors.primary,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   buttonText: {
-    color: 'white'
+    color: 'white',
   },
   locationContainer: {
     padding: 10,
     borderBottomColor: colors.primary,
-    borderBottomWidth: 2
+    borderBottomWidth: 2,
   },
   locationName: {
-    fontSize: 20
+    fontSize: 20,
   },
   locationInfo: {
-    color: 'rgba(0, 0, 0, .5)'
-  }
-})
+    color: 'rgba(0, 0, 0, .5)',
+  },
+});
 
-export default City
+export default City;
